c55be42add1077338aa9e3f2d9d910dd3476c939
JiangXiaoqiang <jiangtingqiang@gmail.com>
2023-06-05 17:44:33 +0800

    fix: fixed the config issue

--- src/config/app/config-pro.ts
+++ src/config/app/config-pro.ts
@@ -6,7 +6,7 @@ let proConfigMap = new Map([
   ["accessTokenUrlPath","/cvpub/auth/access-token/refresh"],
   ["appHome","https://cv.poemhub.top"],
   ["appId" , "xSW5a4BbVB"],
-  ["defaultPrjDutyPrompt",""]
+  ["cvBaseUrl","https://cv.poemhub.top/cv/static/pdf/"]
 ]); 
 
 export default proConfigMap;
