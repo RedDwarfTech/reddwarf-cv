5fae7a6614ff3cdab6fc15f981723075904b4348
JiangXiaoqiang <jiangtingqiang@gmail.com>
2023-06-08 12:57:16 +0800

    refactor: change to reset password api

--- src/service/user/CvUserService.ts
+++ src/service/user/CvUserService.ts
@@ -4,7 +4,7 @@ import { UserActionType, XHRClient } from "rd-component";
 export function doSendVerifyCode(params: any) {
     const config = {
         method: 'post',
-        url: "/cvpub/user/sms",
+        url: "/cvpub/user/sms/reset-pwd",
         data: JSON.stringify(params)
     };
     const actionTypeString: string = UserActionType[7];
